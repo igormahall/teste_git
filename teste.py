05525f21a0b8d567609cf75ab7a5867650698542
Igor <igormahall@gmail.com>
2025-05-08 19:28:44 -0400

    Programinha que calcula respostas do exercicio

--- teste.py
+++ teste.py
@@ -1,7 +1,8 @@
 # print("Hello World")
 # print("Hello World2")
+from django.template.defaultfilters import length
 
- # Revisão Herança (base para o Django: classe, herança, construtor)
+# Revisão Herança (base para o Django: classe, herança, construtor)
 
 # # 1) Herança simples
 # class Animal:
@@ -47,26 +48,39 @@
 
 # 4) Herança e injecao de dependencia
 
-class Veiculo:
-    def mover(self):
-        pass
+# class Veiculo:
+#     def mover(self):
+#         pass
+#
+# class Motor:
+#     def partida(self):
+#         print("Vrumm")
+#     def desliga(self):
+#         print("Desligando")
+#
+# class Carro(Veiculo):
+#     def __init__(self, motor):
+#         self.motor = motor
+#     def mover(self):
+#         self.motor.partida()
+#         print("Veiculo se movendo")
+#     def parar(self):
+#         self.motor.desliga()
+#
+# m = Motor()
+# c = Carro(m)
+# c. mover()
+# c.parar()
 
-class Motor:
-    def partida(self):
-        print("Vrumm")
-    def desliga(self):
-        print("Desligando")
 
-class Carro(Veiculo):
-    def __init__(self, motor):
-        self.motor = motor
-    def mover(self):
-        self.motor.partida()
-        print("Veiculo se movendo")
-    def parar(self):
-        self.motor.desliga()
+# Programinha que calcula as respostas corretas do exercicio online
 
-m = Motor()
-c = Carro(m)
-c. mover()
-c.parar()
+def resp_correta(teste):
+    gabarito = ['b', 'a', 'c', 'a', 'd', 'd', 'd', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'd', 'c', 'b', 'd', 'd', 'b']
+    total = len(gabarito)
+    valor = sum(1 for r1, r2 in zip(gabarito,teste) if r1==r2)
+    porcentagem = valor / total * 100
+    return f"{valor}/{total} = {porcentagem}%"
+
+teste = ['b','a','c','a','d','d','d','a','b','a','c','a','b','a','d','c','b','d','d','b']
+print(resp_correta(teste))
